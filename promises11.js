1aa861e86e9c8cef9db5d1ebe646b0ce24d8dd8a
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-15 13:07:36 -0700

    finished a few exrcises

--- promises11.js
+++ promises11.js
@@ -1,9 +1,23 @@
 
 var axios = require('axios');
-axios.get('https://pokeapi.co/api/v2/pokemon/').then((response)=>{
-    for(var something in response){
-        if(response[something].name){
-        console.log(response[something].name)
-        }
-    }
+
+let test1 = axios.get('https://pokeapi.co/api/v2/pokemon/1/').then((response)=>{
+    new Promise((resolve, reject)=>{
+        return response.data.name
+});
+
+let test2 = axios.get('https://pokeapi.co/api/v2/pokemon/2/').then((response)=>{
+    new Promise((resolve, reject)=>{
+        return response.data.name
+});
+
+let test3 = axios.get('https://pokeapi.co/api/v2/pokemon/3/').then((response)=>{
+    new Promise((resolve, reject)=>{
+        return response.data.name
+});
+
+let allPokemon = [test1, test2, test3];
+
+Promise.all(allPokemon).then((values)=>{
+    console.log(`${values[0]}, ${values[1]}, ${values[2]}`);
 })
